b5ff19f6c5c25a168b44ed3fb7f5d8ea3a1699a7
eltallerjsdev <64107536+eltallerjsdev@users.noreply.github.com>
2021-02-15 18:34:44 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    location: 'Crazy Rd. 123',
+  };
+
+  handleSubmit = async e => {
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.response}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.location}
+            onChange={e => this.setState({ location: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
-
 export default App;
