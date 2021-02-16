d9ea116e7531dee7af29b70bf27c6000ee4dbda3
santiagovj22 <iefaz.santiagovalle@gmail.com>
2021-02-15 21:55:34 -0500

    add request function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-class App extends Component {
 
-  state = {
-    location: 'Crazy Rd. 123',
+
+export default function App() {
+
+  const [location, setLocation] = useState('')
+  // const [response, setResponse] = useState({})
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    console.log(location)
+    let res = await fetch('http://systec-puzzle.herokuapp.com/lightbeam', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({location})
+    })
+    .catch(err=>console.warn(err))
+    console.log(res)
+    // if(res.ok) setResponse(res)
   };
 
-  handleSubmit = async e => {
-  };
+  const handleChange = (e) => setLocation(e.target.value)
 
-  render() {
     return (
       <div className="App">
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
+        {/* <p>{response}</p> */}
+        <form onSubmit={handleSubmit}>
           <p>
             <strong>Post to Server:</strong>
           </p>
           <input
             type="text"
-            value={this.state.location}
-            onChange={e => this.setState({ location: e.target.value })}
+            value={location}
+            onChange={handleChange}
           />
           <button type="submit">Submit</button>
         </form>
       </div>
     );
-  }
 }
-export default App;
