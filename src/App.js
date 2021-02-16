060a505fe2479967504a8a6b02a27a5800acb2ca
OsirisRoman <osirisr1994@gmail.com>
2021-02-15 23:45:25 -0500

    Cleaning and refactoring the post method

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import React, { useState } from 'react';
-import './App.css';
-import Button from './components/Button';
-import Input from './components/Input';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
-  const [location, setLocation] = useState('Crazy Rd. 123');
-  const [response, setResponse] = useState('');
+    const remoteServer = "https://systec-puzzle.herokuapp.com/lightbeam";
+    const [location, setLocation] = useState("Crazy Rd. 123");
+    const [response, setResponse] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post(remoteServer, {
+                location: location,
+            })
+            .then((res) => setResponse(res))
+            .catch((err) => console.log(err));
+    };
 
-  return (
-    <div className="App">
-        <p>{response}</p>
-        {/*<form method='POST' action='https://systec-puzzle.herokuapp.com/lightbeam' onSubmit={(e) => handleSubmit(e)}>*/}
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            name='location'
-            type="text"
-            value={location}
-            onChange={e => setLocation(e.target.value)}
-          />
-          <Input />
-          <Button />
-          <button type="submit">Submit</button>
-        </form>
-    </div>
-  );
-}
+    return (
+        <div className='App'>
+            <p>{response}</p>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <p>
+                    <strong>Post to Server:</strong>
+                </p>
+                <input
+                    name='location'
+                    type='text'
+                    value={location}
+                    onChange={(e) => setLocation(e.target.value)}
+                />
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    );
+};
 
 export default App;
